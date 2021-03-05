f838b4c6a32896c4032716263ffebc3e3fbb3e85
Marek <wm41648@zut.edu.pl>
2021-03-05 11:32:24 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <header>
+      <div v-if='status == false' class="nav">
+          <button class="button"><router-link class="nav_reg" to="/register">Register</router-link></button>
+          <button  class="button"> <router-link class="nav_log"  to="/login">Login</router-link></button>
+      </div>
+    </header>
+      <br>
+      <br>
+      <router-view></router-view>
+
+    </div>
+    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+     status: false,
+    }
+  },
+  mounted() {
+    console.log(this.status)
+    if(sessionStorage.length) {
+      this.status = sessionStorage.getItem("logged in")
+    }
+  },
 }
+
 </script>
 
 <style>
 #app {
+  background-color: #37cec0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+p{
+  color:white;
+  font-weight: bold;
+}
+
+.button {
+  background-color: #37cec0;
+  border: 3px solid white;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 29px;
+}
+
+.nav_reg{
+  margin-left: 4px;
+  text-decoration: none;
+  color:white;
+  font-weight: bold;
+}
+
+.nav_log{
+  margin-left: 6px;
+  text-decoration: none;
+  color:white;
+  font-weight: bold;
 }
 </style>
